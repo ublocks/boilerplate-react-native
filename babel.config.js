2cc1978946213ec746b8139f8b7101a705b4781c
Kuyen <iamcxa@gmail.com>
2019-10-09 16:24:40 +0800

    fix: 🐛 fix build bundle error

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
   env: {
-    'production': {
-      'plugins': ['transform-remove-console', { 'exclude': [ 'error', 'warn'] }]
+    production: {
+      plugins: ['transform-remove-console'],
     },
-  }
-}
+  },
+};
