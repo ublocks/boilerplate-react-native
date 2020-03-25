3c0b8ab1cfc04f163425b6d477e8729f697e0f1b
Kuyen <iamcxa@gmail.com>
2020-03-25 12:31:50 +0800

    refactor: code style

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,7 +9,7 @@ import App from 'App/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-jest.mock('react-native-localize', ()=>{})
+jest.mock('react-native-localize', () => {});
 
 jest.useFakeTimers();
 
